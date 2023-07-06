32db90806d033b008ca82f7fac3514e3022a1d26
Niroop.N <niroop.n@karkinos.in>
2023-07-06 15:27:19 +0530

    MHEALTH-3 : Create a customizable theme for the m-Health app

---
+++ lib/config/router/router_transition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/// Animation for the Material routing. we have set it as default in app theme.
+class CustomTransitionBuilder extends PageTransitionsBuilder {
+  const CustomTransitionBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+      PageRoute<T> route,
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child,
+      ) {
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: Offset.zero,
+      ).animate(animation),
+      child: child,
+    );
+  }
+}
