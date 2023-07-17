2c924d0b03e191480a5c224f064b1367bb9288bb
Niroop.N <niroop.n@karkinos.in>
2023-07-17 12:03:15 +0530

    MHEALTH-10, MHEALTH-13 & MHEALTH-16 : Create customizable alert dialog in m-Health App, Create Circular Avatar Field in m-Health App & Check Internet Connectivity on Splash Screen in Flutter App

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -12,3 +12,5 @@ enum LoginScreenTypes { EMAIL, MOBILE_NUMBER, OTP_SCREEN }
 enum NetworkStatus { online, offline }
 
 enum LocationPermissionStatus { SERVICE_DISABLED, GRANTED, WHILE_IN_USE, DENIED, FOREVER_DENIED }
+
+enum CircularAvatarFieldChildType { TEXT, SVG_ASSET, IMAGE_ASSET }
--- lib/widgets/circular_avatar_widget.dart
+++ lib/widgets/circular_avatar_widget.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mhealth/utils/app_color_scheme.dart';
 import 'package:mhealth/utils/app_styles.dart';
+import 'package:mhealth/utils/enums.dart';
 
 class CircularAvatar extends StatelessWidget {
-
-  /// [letter] is used as the initials of the first and last name for display if the profile pic isn't there
-  final String letter;
-
   /// [backgroundColor] is being used to show in the background of the circle avatar widget
   /// [textColor] is used to show the color of the text/letters
   /// [borderColor] is used to show the color of the border of the circle
@@ -15,10 +14,21 @@ class CircularAvatar extends StatelessWidget {
   /// [borderWidth] is being used to give the size of the border of the circle
   final double radius, borderWidth;
 
+  /// we are supporting 3 types of child declared in this enum [CircularAvatarFieldChildType]
+  /// 1. if you want to set Text as child then pass [childType] as [CircularAvatarFieldChildType.TEXT]
+  /// 2. if you want to set SvgAsset as child then pass [childType] as [CircularAvatarFieldChildType.SVG_ASSET]
+  /// 3. if you want to set Image Asset as child then pass [childType] as [CircularAvatarFieldChildType.IMAGE_ASSET]
+  final CircularAvatarFieldChildType childType;
+
+  /// if [childType] is set as [CircularAvatarFieldChildType.TEXT] then pass text of prefix
+  /// if [childType] is set as [CircularAvatarFieldChildType.SVG_ASSET] or [CircularAvatarFieldChildType.IMAGE_ASSET] then pass asset path of icon
+  final String childData;
+
   const CircularAvatar({
     super.key,
-    required this.letter,
-    this.backgroundColor = Colors.blue,
+    required this.childType,
+    required this.childData,
+    this.backgroundColor = AppColorScheme.kPrimaryColor,
     this.textColor = Colors.white,
     this.radius = 40.0,
     this.borderWidth = 2.0,
@@ -33,9 +43,19 @@ class CircularAvatar extends StatelessWidget {
       child: Stack(
         children: [
           Center(
-            child: Text(
-              letter,
-              style: AppStyles.bodyMedium.copyWith(color: textColor),
+            child: Builder(
+              builder: (context) {
+                switch (childType) {
+                  case CircularAvatarFieldChildType.TEXT:
+                    return Text(childData, style: AppStyles.bodyMedium.copyWith(color: textColor));
+                  case CircularAvatarFieldChildType.SVG_ASSET:
+                    return SvgPicture.asset(childData, colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn));
+                  case CircularAvatarFieldChildType.IMAGE_ASSET:
+                    return Image.asset(childData);
+                  default:
+                    return const SizedBox();
+                }
+              },
             ),
           ),
           Positioned.fill(
