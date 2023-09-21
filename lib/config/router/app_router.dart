3ab1fe4cd57d27d43c676f337ee9f5aa4c387630
Sanjay Singha <sanjay.singha@karkinos.in>
2023-09-21 12:20:47 +0530

    #84: Refactoring Seamless user Log-in in the m-Health App

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -122,6 +122,9 @@ class AppRouter {
           if (navigationRoute == LoginHome.routerPath) {
             return LanguageSelectionScreen.routerPath;
           }
+          if (navigationRoute == SplashScreen.routerPath) {
+            return DashboardScreen.routerPath;
+          }
         }
 
         /// if user is logged we are null so that it will navigate from one screen to another without any redirect
--- lib/utils/app_values.dart
+++ lib/utils/app_values.dart
@@ -12,7 +12,7 @@ class AppValues {
 
   static TextInputFormatter get numberInputFormatter => CustomInputFormatter(regx: r'^[0-9]*$');
   static TextInputFormatter get mobileInputFormatter => MaskTextInputFormatter(mask: '##########');
-  static TextInputFormatter get emailInputFormatter => CustomInputFormatter(regx: r'^[a-zA-Z0-9-._@]*$');
+  static TextInputFormatter get emailInputFormatter => CustomInputFormatter(regx: r'^[a-zA-Z0-9-._+@]*$');
   static TextInputFormatter get stringInputFormatter => CustomInputFormatter(regx: r'^[a-zA-Z]*$');
 
   static int get kOtpTimer => 30;
